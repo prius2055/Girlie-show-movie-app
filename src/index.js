33a42828a4be8efbd986dcf252161c52ac33b1b7
prius2055 <prius2055@gmail.com>
2023-04-18 20:11:13 +0100

    Create a branch to list the items

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
 import './style.css';
+import likeButton from './img/heart.png'
+const homepage = document.getElementById('homepage');
+homepage.innerHTML = '';
+
+window.addEventListener('DOMContentLoaded', () => {
+  let shows;
+  const fetchMemes = async () => {
+    const response = await fetch('https://api.tvmaze.com/search/shows?q=girls');
+    const results = await response.json();
+    shows = results
+    console.log(shows);
+
+    // for (let i = 1; i < 7; i++) {
+    //   homepage.innerHTML += `<div class='show'>
+    //     <img src='${shows[i].url}' alt='${shows[i].name}' width='250px' height='200px' />
+    //     <div class='show-name'>
+    //       <p>${shows[i].name}</p>
+    //       <div class='show-likes'>
+    //         <img src='${likeButton}'/>
+    //         <p>3 likes</p>
+    //       </div>
+    //     </div>
+    //     <button>Comments</button>
+    //     <button>Reservations</button>
+    //     </div>`;
+    // }
+  };
+
+  fetchMemes();
+});
--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,6 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -20,6 +19,11 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
+      },
+
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
       },
     ],
   },
