8023e41cf8e16098da7b447fa0bfca147bfb975e
prius2055 <prius2055@gmail.com>
2023-04-18 20:20:20 +0100

    Add the TV show API

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './style.css';
-import likeButton from './img/heart.png'
+import likeButton from './img/heart.png';
 const homepage = document.getElementById('homepage');
 homepage.innerHTML = '';
 
@@ -8,23 +8,23 @@ window.addEventListener('DOMContentLoaded', () => {
   const fetchMemes = async () => {
     const response = await fetch('https://api.tvmaze.com/search/shows?q=girls');
     const results = await response.json();
-    shows = results
+    shows = results;
     console.log(shows);
 
-    // for (let i = 1; i < 7; i++) {
-    //   homepage.innerHTML += `<div class='show'>
-    //     <img src='${shows[i].url}' alt='${shows[i].name}' width='250px' height='200px' />
-    //     <div class='show-name'>
-    //       <p>${shows[i].name}</p>
-    //       <div class='show-likes'>
-    //         <img src='${likeButton}'/>
-    //         <p>3 likes</p>
-    //       </div>
-    //     </div>
-    //     <button>Comments</button>
-    //     <button>Reservations</button>
-    //     </div>`;
-    // }
+    for (let i = 1; i < 7; i++) {
+      homepage.innerHTML += `<div class='show'>
+        <img src='${shows[i].show.image.original}' alt='${shows[i].show.name}' width='250px' height='200px' />
+        <div class='show-name'>
+          <p>${shows[i].show.name}</p>
+          <div class='show-likes'>
+            <img src='${likeButton}'/>
+            <p>3 likes</p>
+          </div>
+        </div>
+        <button>Comments</button>
+        <button>Reservations</button>
+        </div>`;
+    }
   };
 
   fetchMemes();
